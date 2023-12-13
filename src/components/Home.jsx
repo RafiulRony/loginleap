36db2a5b4fa361c00236423ac594a09d7a61f89d
rafiul-islam <rafiulrony.cse001@gmail.com>
2023-12-13 15:32:10 +0600

    practice callback and asynchronous

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,37 @@ import useWindowWidth from './Hooks/useWindowWidth'
 export default function Home() {
   const onSmallScreen = useWindowWidth()
 
+  var numbers = [1,2,3,4,5,6,7];
+
+  // var st = `[${String(numbers)}]`;
+  var st = [String(numbers)]
+  console.log(st);
+  var sum = numbers.reduce((previousValue, currentValue)=>{
+    return previousValue+currentValue;
+  }, 0)
+  console.log(sum);
+
+  function myDisplayer(some){
+    console.log(some);
+  }
+  function myCalculator(num1, num2, myCallback){
+    var result = num1 + num2;
+    myCallback(result);
+  }
+  myCalculator(3,4,myDisplayer)
+
+  setInterval(function() {hello()}, 4000);
+
+  function hello() {
+    let d = new Date();
+  var Time=
+  d.getHours() + ":" +
+  d.getMinutes() + ":" +
+  d.getSeconds();
+  console.log(Time);
+  }
+  
+
   return (
     <>
     <h2>Home Page</h2>
